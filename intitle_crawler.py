f28408dcbad3f7e33551955cd2867ebe1758df72
Chetanck <chetan.k2626@gmail.com>
2018-09-07 02:48:04 +0530

    commit for web

---
+++ intitle_crawler.py
@@ -0,0 +1,40 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+import pymysql as MySQLdb
+
+
+#url = 'https://theswissbay.ch/pdf/Gentoomen%20Library/Programming/Python/'
+
+db = MySQLdb.connect("localhost","root","root","books")
+cursor = db.cursor()
+
+my_url = []
+
+print ("Enter index URL to get information or exit to Exit:")
+while True:
+	urls = input()
+	if urls == "exit" or urls == "EXIT":
+		break
+	else:
+		my_url.append(urls)
+
+
+for url in my_url:
+	try:
+		uClient = uReq(url)
+		page_html = uClient.read()
+		uClient.close()
+		page_soup = soup(page_html, "html.parser")
+		links = page_soup.findAll("a",href=True)
+	except:
+		print("This ended in an error ----> ",url)
+
+	for link in links:
+		completeLink = url+link["href"]
+		sql = "insert into book_links(book_name,book_link) values(\"%s\",\"%s\")" % (str(link.text),str(completeLink)) 
+		try:
+			cursor.execute(sql)
+			db.commit()
+		except:
+			print("This ended in an error ----> ",link.text , " ------>",completeLink)
+db.close()
